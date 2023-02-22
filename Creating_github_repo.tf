f5449a1a49e3fa5a8b44302667839c1d6371e5ef
sugaliudaykiran <70463117+sugaliudaykiran@users.noreply.github.com>
2023-02-22 18:02:54 +0530

    Add files via upload

---
+++ Creating_github_repo.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    github = {
+      source = "integrations/github"
+      version = "5.18.0"
+    }
+  }
+}
+
+# Configure the Github provider
+provider "github" {
+    token = "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"
+}
+
+resource "github_repository" "Terra" {
+    name = "Terraform"
+    description = "Terraform lab work"
+
+    private = false
+}
